48112f56a93193e72bcc8be8e62e168de243d350
MagoPrismo <sgngms3@gmail.com>
2025-09-12 20:30:31 -0300

    ex 05

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
+        WelcomeMessage();
+
+        string name = UserName();
+        int number = UserNumber();
+
+        int square = SquareNumber(number);
+
+        FinalMessage(name, square);
+    }
+
+    static void WelcomeMessage()
+    {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+    }
+
+    static string UserName()
+    {
+        Console.Write("What's your name? ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int UserNumber()
+    {
+        Console.Write("What's your favorite number? ");
+        int x = int.Parse(Console.ReadLine());
+        
+        return x;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int result = number * number;
+
+        return result;
+    }
+
+    static void FinalMessage(string name, int square)
+    {
+        Console.WriteLine($"Your name is {name}, and the square of you {square}");
     }
 }
