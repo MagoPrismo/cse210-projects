8568e02244c70872cd810c503edba5447d200ec4
MagoPrismo <sgngms3@gmail.com>
2025-09-12 13:56:55 -0300

    ex 03

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,27 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+                Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int number = -1;
+
+        while (number != magicNumber)
+        {
+            Console.Write("What's your guess? ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
+        Console.WriteLine($"You guessed it!! The Magic Number is {magicNumber}");
     }
 }
