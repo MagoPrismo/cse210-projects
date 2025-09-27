3aa220dc231bb8bb1f4ef6635b6eb55206ccf794
MagoPrismo <sgngms3@gmail.com>
2025-09-27 14:25:06 -0300

    fraction

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction numb1 = new Fraction();
+        string result1 = numb1.GetFractionString();
+        double result2 = numb1.GetDecimalValue();
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+
+        Fraction numb2 = new Fraction(5);
+        string result3 = numb2.GetFractionString();
+        double result4 = numb2.GetDecimalValue();
+        Console.WriteLine(result3);
+        Console.WriteLine(result4);
+
+        Fraction numb3 = new Fraction(1,3);
+        string result5 = numb3.GetFractionString();
+        double result6 = numb3.GetDecimalValue();
+        Console.WriteLine(result5);
+        Console.WriteLine(result6);
     }
 }
